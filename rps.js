c3e3a0ebdfc7d3013307aef3fa3c3a2a4cde781a
rastamypasta <38663414+rastamypasta@users.noreply.github.com>
2019-02-25 18:40:32 -0700

    Updated code to use eventListeners
    Updated code to use eventListeners and functions instead of just responding to onClicks.

--- rps.js
+++ rps.js
@@ -1,35 +1,122 @@
-// Start Button Control
+// Variables from HTML Ids
+const start = document.getElementById('start-button');
+const scoreBox = document.getElementById('scores-container');
+const btns = document.getElementById('buttons-container');
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const match = document.getElementById('pvsc-container');
+let playerImg = document.getElementById('player-img');
+let compImg = document.getElementById('comp-img');
+const matchImg = document.getElementsByClassName('match-img');
+let prntPlayerScore = document.getElementById('player-score');
+let prntCompScore = document.getElementById('computer-score');
 
-// Assigns CSS class 'start-button' to the JS variable startBtn 
-const startBtn = document.getElementById('start-button');
+// Score variables
+playerScore = 0;
+compScore = 0;
 
-// Start-button actions when clicked
-startBtn.onclick = function() {
-    // Hides the start button
-    document.getElementById('start-button').style.display = 'none';
-    // Shows the score container
-    document.getElementById('scores-container').style.display = 'inline-flex';
-    // shows the buttons container for user prompt
-    document.getElementById('buttons-container').style.display = 'flex';
+// Start game function
+start.addEventListener('click', function() {
+    // Hides start button
+    this.style.display = 'none';
+    // Shows score box
+    scoreBox.style.display = 'inline-flex';
+    // shows user choice buttons
+    btns.style.display = 'inline';
+    // reset scores
+    playerScore = 0;
+    compScore = 0;
+})
+
+// Functions to listen for the player choice
+
+// Player choice variables
+let playerChoice;
+
+// RPS Button event listeners
+rock.addEventListener('click', () => {
+    playerChoice = 'rock';
+    playerImg.src = 'https://raw.githubusercontent.com/BabsLabs/Rock-Paper-Scissors/master/Rock.png';
+});
+
+paper.addEventListener('click', () => {
+    playerChoice = 'paper';
+    playerImg.src = 'https://raw.githubusercontent.com/BabsLabs/Rock-Paper-Scissors/master/Paper.png';
+});
+
+scissors.addEventListener('click', () => {
+    playerChoice = 'scissors';
+    playerImg.src = 'https://raw.githubusercontent.com/BabsLabs/Rock-Paper-Scissors/master/Scissors.png';
+});
+
+// Start match vs computer upon player choice
+btns.addEventListener('click', () => {
+    btnHider();
+    getCompChoice();
+    checkMatch();
+});
+
+// Function to hide buttons and show match
+function btnHider() {
+    btns.style.display = 'none';
+    match.style.display = 'flex';
 };
 
-// Once player input received hide player choice buttons
-const playerChoice = document.getElementById('rpsbutton');
 
-// Define a fucntion to store player input
+// Calculate computer choice
+let compChoice;
 
-function playerInput() {
+// Computer choice function
+function getCompChoice() {
+    // Gets whole number between 1 and 3 (okay betwwen 0 and 2)
+    compChoice = Math.floor(Math.random() * 3);
+    // Assigns the random numer to a string for later camparison with the player choice
+    if (compChoice == 0) {
+        compChoice = 'rock';
+        compImg.src = 'https://raw.githubusercontent.com/BabsLabs/Rock-Paper-Scissors/master/Rock.png';
+    } else if (compChoice == 1) {
+        compChoice = 'paper';
+        compImg.src = 'https://raw.githubusercontent.com/BabsLabs/Rock-Paper-Scissors/master/Paper.png';
+    } else if (compChoice == 2) {
+        compChoice = 'scissors';
+        compImg.src = 'https://raw.githubusercontent.com/BabsLabs/Rock-Paper-Scissors/master/Scissors.png';
+    }
+    console.log(playerChoice + " " + compChoice);
+};
 
-}
+// Check the player choice vs the comp choice
 
-// Define a funciton to hide the choice buttons
+// Delay the Alert window
 
-// Define a function to make players choice
 
-// Radio button actions on click
-playerChoice.onclick = function() {
-    // Hides player buttons;
-    document.getElementsByName('buttons-container').style.display = 'none';
-    // Shows player vs computer container
-    document.getElementById('pvsc-container').style.display = 'flex';
+// Function to check game result
+function checkMatch() {
+    // Check for tie game
+    if (playerChoice == compChoice) {
+        alert('Tie Game!');
+        // Check for player or computer win options 
+    } else if (playerChoice == 'rock' && compChoice == 'scissors') {
+        alert('Player Wins!');
+        playerScore++;
+    } else if (playerChoice == 'paper' && compChoice == 'scissors') {
+        alert('Computer Wins!');
+        compScore++;
+    } else if (playerChoice == 'scissors' && compChoice == 'rock') {
+        alert('Computer Wins!');
+        compScore++;
+    } else if (playerChoice == 'paper' && compChoice == 'rock') {
+        alert('Player Wins!');
+        playerScore++;
+    } else if (playerChoice == 'scissors' && compChoice == 'paper') {
+        alert('Player Wins!');
+        playerScore++;
+    } else if (playerChoice == 'rock' && compChoice == 'paper') {
+        alert('Computer Wins!');
+        compScore++;
+    }
+
+    console.log(playerScore + " " + compScore);
+    prntPlayerScore.innerHTML = playerScore;
+    prntCompScore.innerHTML = compScore;
 };
